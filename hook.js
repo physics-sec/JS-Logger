b65853068e438def07b7b9f4b3cb027b89922530
physics-sp <physics-sp@protonmail.com>
2020-01-26 23:53:09 -0300

    bug fix in proxying Element.prototype some attributes result in an exception, added try catch

--- hook.js
+++ hook.js
@@ -52,53 +52,41 @@ dont_hook = [
 	"postMessage"
 ]
 
-for (var prop in this)
-{
-	if (this[prop] instanceof Function && !dont_hook.includes(this[prop].name))
-	{
-		this[prop] = new Proxy(this[prop], apply_handle);
-
-	}
-}
+for (var name in this)
+	if (this[name] instanceof Function && !dont_hook.includes(this[name].name))
+		this[name] = new Proxy(this[name], apply_handle);
 
 var names = [];
 for (name in document)
-{
 	names.push(name);
-}
 
 for (var i = names.length - 1; i >= 0; i--)
-{
 	if (document[names[i]] instanceof Function && !dont_hook.includes(names[i]))
-	{
 		document[names[i]] = new Proxy(document[names[i]], apply_handle);
-	}
-}
 
 var names = [];
 for (name in window)
-{
 	names.push(name);
-}
 
 for (var i = names.length - 1; i >= 0; i--)
-{
 	if (window[names[i]] instanceof Function && !dont_hook.includes(names[i]))
-	{
 		window[names[i]] = new Proxy(window[names[i]], apply_handle);
-	}
-}
 
 var names = [];
 for (name in Element.prototype)
-{
 	names.push(name);
-}
 
 for (var i = names.length - 1; i >= 0; i--)
 {
-	if (Element.prototype[names[i]] instanceof Function && !dont_hook.includes(names[i]))
-	{
-		Element.prototype[names[i]] = new Proxy(Element.prototype[names[i]], apply_handle);
+	try {
+		if (Element.prototype[names[i]] instanceof Function && !dont_hook.includes(names[i]))
+		{
+			console.log(names[i]);
+			Element.prototype[names[i]] = new Proxy(Element.prototype[names[i]], apply_handle);
+		}
+	}
+	catch(err) {
+		continue
 	}
 }
+
